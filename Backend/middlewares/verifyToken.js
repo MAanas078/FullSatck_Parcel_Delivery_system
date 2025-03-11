ab1adaffdc6d77999c2d56aaac3e43f0cb02eca8
Mohd Aanas <131160846+MAanas078@users.noreply.github.com>
2025-03-11 14:21:57 +0530

    Update verifyToken.js

--- Backend/middlewares/verifyToken.js
+++ Backend/middlewares/verifyToken.js
@@ -6,7 +6,7 @@ const verifyToken = (req, res, next) => {
 
   if (authHeader) {
     const token = authHeader.split("")[1];
-    jwt.verify(token, SpeechRecognitionResultList.env.JWT_SEC, (err, user) => {
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
       if (err) res.status(403).json("Token is not valid");
       req.user = user;
       next();
